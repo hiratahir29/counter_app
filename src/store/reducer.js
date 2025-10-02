f31ac1e77c4712d963aa1231c3b98737d3e376c0
hiratahir29 <hirab.299@gmail.com>
2025-10-01 23:31:04 -0400

    Implemented Reducer

---
+++ src/store/reducer.js
@@ -0,0 +1,40 @@
+const initialState={
+    counter:0
+}
+
+
+const reducer=(state=initialState,action)=>{
+    if(action.type==='INCREMENT')
+    {
+        return{
+            ...state,
+            counter:state.counter+1
+        }
+    }
+    if(action.type==='DECREMENT')
+    {
+        return{
+            ...state,
+            counter:state.counter-1
+        }
+    }
+    if(action.type==='INCREMENTby5')
+    {
+        return{
+            ...state,
+            counter:state.counter+action.value
+        }
+    }
+    if(action.type==='DECREMENTby5')
+    {
+        return{
+            ...state,
+            counter:state.counter-action.value
+        }
+    }
+    
+    return state;
+}
+
+
+export default reducer;
