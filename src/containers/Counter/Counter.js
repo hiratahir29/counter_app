890edc86867ceb1b923b37eb36e8ada9096ca3d1
hiratahir29 <hirab.299@gmail.com>
2025-10-01 23:37:55 -0400

    dispatched & received actions

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import { connect } from 'react-redux';
 
 class Counter extends Component {
     state = {
         counter: 0
     }
-
     counterChangedHandler = ( action, value ) => {
         switch ( action ) {
             case 'inc':
@@ -27,17 +27,30 @@ class Counter extends Component {
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={()=>this.counterChangedHandler('inc',1)} />
-                <CounterControl label="Decrement" clicked={()=>this.counterChangedHandler('dec',1)}  />
-                <CounterControl label="Add 5" clicked={()=>this.counterChangedHandler('add',5)}  />
-                <CounterControl label="Subtract 5" clicked={()=>this.counterChangedHandler('sub',5)}  />
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.incBy1} />
+                <CounterControl label="Decrement" clicked={this.props.decBy1}  />
+                <CounterControl label="Add 5" clicked={this.props.incBy5}  />
+                <CounterControl label="Subtract 5" clicked={this.props.decBy5}  />
 
             </div>
         );
     }
 }
+const mapDispatchToProps=dispatch=>{
+    return{
+        incBy1:()=>dispatch({type:'INCREMENT'}),
+        decBy1:()=>dispatch({type:'DECREMENT'}),
+        incBy5:()=>dispatch({type:'INCREMENTby5',value:5}),
+        decBy5:()=>dispatch({type:'DECREMENTby5',value:5}),
+    }
+}
 
+const mapStateToProps=state=>{
+    return{
+        ctr:state.counter
+    }
+}
 
+export default connect(mapStateToProps,mapDispatchToProps)(Counter);
 
-export default Counter;
